429f157903044c4db1ec09e597f129fb2db5a859
Sahil kumar <sahil12344kumar@gmail.com>
2025-07-20 10:21:35 +0530

    Use VITE_API_BASE_URL and update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/", // 👈 Add this line
+//   plugins: [react()],
+// })
+// npm run 
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
+  base: "/", // ⚠️ is line ke baad kuch nahi hona chahiye
   plugins: [react()],
-})
+});
